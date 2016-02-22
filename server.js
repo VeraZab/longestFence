e0a00addb13d82832e6429012d80cff8d0eda168
Vera Zabeida <vera.zabeida@gmail.com>
2016-02-22 10:16:50 -0500

    determine port dynamically

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 var http = require('http');
 var onRequest = require('./routes');
+var port = process.argv[2] || 8888;
 
 function start(){
-	http.createServer(onRequest).listen(8888);
+	http.createServer(onRequest).listen(port);
 	console.log("Server has started.");
 }
-
+console.log(process.argv[2]);
+console.log(port);
 module.exports = start;

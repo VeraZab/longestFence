fdb6e619c367992f5a16bb448dcf523abc45b538
Vera Zabeida <vera.zabeida@gmail.com>
2016-02-22 10:26:35 -0500

    cwd specified

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 var http = require('http');
-var onRequest = require('./routes');
+process.env.PWD = process.cwd();
+var onRequest = require(process.env.PWD+'/routes');
+
 var port = process.argv[2] || 8888;
 
 function start(){
@@ -7,5 +9,7 @@ function start(){
 	console.log("Server has started.");
 }
 console.log(process.argv[2]);
+console.log(process.cwd());
 console.log(port);
+
 module.exports = start;

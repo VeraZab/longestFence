d746e553036760f7e43bbb37a420f15e61c7dc27
Vera Zabeida <vera.zabeida@gmail.com>
2016-04-03 17:29:10 -0400

    test original images

--- staticAssets/scripts/view.js
+++ staticAssets/scripts/view.js
@@ -15,7 +15,7 @@
 		function preloadImage(x){
 			var picture = loadImage(x);
 
-			if(x===20){
+			if(x===10){
 				picture.onload = function(){
 					fences.removeChild(loader);
 					var end = new Date().getTime();
@@ -24,14 +24,14 @@
 			}
 		};
 
-		for(var x = 1; x<=20; x++){
+		for(var x = 1; x<=10; x++){
 			preloadImage(x);
 		};
 	};
 
 	function loadImage(x){
 		var picture = document.createElement('img');
-		picture.src = '/images/'+x+'.jpeg';
+		picture.src = '/images/'+x+'.jpg';
 		return picture;
 	};
 
@@ -81,7 +81,7 @@
 			slider.appendChild(a);
 		};
 
-		for(var x = 1; x<=20; x++){
+		for(var x = 1; x<=10; x++){
 			createImage(x);
 		};
 
@@ -283,48 +283,28 @@
 
 	var data = {
 		countries: [
-			['albania','tirana'],
-			['uganda','kampala'],
-			['egypt','cairo'],
-			['togo', 'lome'],
-			['haiti', 'port-au-prince'],
-			['denmark', 'copenhagen'],
-			['swaziland', 'mbabane'],
-			['panama', 'panama city'],
-			['bahrain', 'manama'],
-			['spain', 'madrid'],
-			['san_marino', 'san marino'],
-			['south_sudan', 'suba'],
-			['suriname', 'paramaribo'],
-			['moldova', 'chrisinau'],
-			['russia', 'moscow'],
-			['jamaica', 'kingston'],
-			['brunei_seri', 'begawan'],
-			['croatia', 'zagreb'],
-			['slovenia', 'ljubljana'],
-			['brazil', 'brasilia']
+			['indonesia', 'jakarta'],
+			['iran','tehran'],
+			['iraq','baghdad'],
+			['ireland','dublin'],
+			['mexico', 'mexico city'],
+			['italy', 'rome'],
+			['south_sudan', 'juba'],
+			['mongolia', 'ulaanbaatar'],
+			['ecuador', 'quito'],
+			['south_korea', 'seoul']
 		],
 		sources: [
-			'https://www.google.ca/maps/@41.32865,19.829555,3a,60y,333.14h,86.95t/data=!3m8!1e1!3m6!1s-GOQNrs2AVwk%2FVN4FeKTIfEI%2FAAAAAAAAKP0%2FQxKJw3ceR90!2e4!3e11!6s%2F%2Flh4.googleusercontent.com%2F-GOQNrs2AVwk%2FVN4FeKTIfEI%2FAAAAAAAAKP0%2FQxKJw3ceR90%2Fw203-h101-n-k-no%2F!7i9728!8i4864!6m1!1e1',
-			'https://www.google.ca/maps/@0.3069588,32.5780787,3a,75y,120.75h,69.43t/data=!3m6!1e1!3m4!1salycf6cLFRphq1JXhHFc5g!2e0!7i13312!8i6656',
-			'https://www.google.ca/maps/place/Cairo,+Cairo+Governorate,+Egypt/@30.0626428,31.24569,3a,30y,241.8h,90.41t/data=!3m8!1e1!3m6!1s-ZBOxCZNBVmg%2FUOgDpST5j3I%2FAAAAAAAAHmg%2FdxTgTQ6A7Es!2e4!3e11!6s%2F%2Flh6.googleusercontent.com%2F-ZBOxCZNBVmg%2FUOgDpST5j3I%2FAAAAAAAAHmg%2FdxTgTQ6A7Es%2Fw277-h100-n-k-no%2F!7i4454!8i1606!4m2!3m1!1s0x14583fa60b21beeb:0x79dfb296e8423bba!6m1!1e1',
-			'https://www.google.ca/maps/place/Lome,+Togo/@6.1887017,1.1607913,3a,37.5y,134.41h,72.42t/data=!3m8!1e1!3m6!1s-8aY7BocHt6s%2FVoFh6ZREYtI%2FAAAAAAAAAFI%2FQvta3hp9H3o!2e4!3e11!6s%2F%2Flh4.googleusercontent.com%2F-8aY7BocHt6s%2FVoFh6ZREYtI%2FAAAAAAAAAFI%2FQvta3hp9H3o%2Fw203-h101-n-k-no%2F!7i10240!8i5120!4m2!3m1!1s0x1023e1c113185419:0x3224b5422caf411d!6m1!1e1',
-			'https://www.google.ca/maps/place/Port-au-Prince,+Haiti/@18.5186036,-72.2911951,3a,90y,47.16h,79.26t/data=!3m8!1e1!3m6!1s-v6tU6THp5Jw%2FVdt38uM6GWI%2FAAAAAAAAG54%2FvZDpibpzyRY!2e4!3e11!6s%2F%2Flh3.googleusercontent.com%2F-v6tU6THp5Jw%2FVdt38uM6GWI%2FAAAAAAAAG54%2FvZDpibpzyRY%2Fw203-h101-n-k-no%2F!7i3584!8i1792!4m2!3m1!1s0x8eb9dd57503eaa91:0x3cd5815df929aa08!6m1!1e1',
-			'https://www.google.ca/maps/place/Copenhagen,+Denmark/@55.6869196,12.5632815,3a,37.5y,34.96h,79.18t/data=!3m7!1e1!3m5!1spEukh4ECJTnEojxVdbS1Sg!2e0!6s%2F%2Fgeo2.ggpht.com%2Fcbk%3Fpanoid%3DpEukh4ECJTnEojxVdbS1Sg%26output%3Dthumbnail%26cb_client%3Dmaps_sv.tactile.gps%26thumb%3D2%26w%3D203%26h%3D100%26yaw%3D34.740959%26pitch%3D0!7i13312!8i6656!4m2!3m1!1s0x4652533c5c803d23:0x4dd7edde69467b8!6m1!1e1',
-			'https://www.google.ca/maps/place/Mbabane,+Swaziland/@-26.3347252,31.1342534,3a,37.5y,19.67h,84.08t/data=!3m7!1e1!3m5!1sReOpWw4Xyc4bQ7N_VskVXA!2e0!6s%2F%2Fgeo3.ggpht.com%2Fcbk%3Fpanoid%3DReOpWw4Xyc4bQ7N_VskVXA%26output%3Dthumbnail%26cb_client%3Dmaps_sv.tactile.gps%26thumb%3D2%26w%3D203%26h%3D100%26yaw%3D27.833216%26pitch%3D0!7i13312!8i6656!4m2!3m1!1s0x1ee8cee147320f07:0x7abf98f27ae1dd9f!6m1!1e1',
-			'https://www.google.ca/maps/place/Panama+City,+Panama/@8.967158,-79.531317,3a,75y,2h,90t/data=!3m8!1e2!3m6!1s97715692!2e1!3e10!6s%2F%2Flh3.googleusercontent.com%2Fproxy%2FANY3MQEJ8kc6z-MkCF4RuqCOi9RvyTS4_U3b8l2PPAVqMXVBFiWulMALMIArNBZMvU2rUsj-qkreB9u8ZK9iTpTfPa5-rQ%3Dw203-h152!7i4000!8i3000!4m2!3m1!1s0x8faca8f1dbe80363:0xaba25df1f042c10e!6m1!1e1',
-			'https://www.google.ca/maps/place/Manama,+Bahrain/@26.2319991,50.5993454,3a,75y,98.48h,79.36t/data=!3m8!1e1!3m6!1s-DegQVeUhNoI%2FVMXSrdwnFrI%2FAAAAAAAAJ6c%2FO736jwNhX6E!2e4!3e11!6s%2F%2Flh4.googleusercontent.com%2F-DegQVeUhNoI%2FVMXSrdwnFrI%2FAAAAAAAAJ6c%2FO736jwNhX6E%2Fw203-h101-n-k-no%2F!7i5632!8i2816!4m2!3m1!1s0x3e49af722776a62d:0x8b6738a6070f60c2!6m1!1e1',
-			'https://www.google.ca/maps/@40.403808,-3.7228856,3a,90y,352.15h,71.15t/data=!3m6!1e1!3m4!1sUhsmviG_r2zxujg8XMZPDw!2e0!7i13312!8i6656',
-			'https://www.google.ca/maps/@43.9714679,12.4731604,3a,90y,207.53h,71.28t/data=!3m6!1e1!3m4!1sIV5bFTziWRHeuaq_MNbb8g!2e0!7i13312!8i6656',
+			'https://www.google.ca/maps/@-6.1722729,106.8130564,3a,90y,165.99h,60.31t/data=!3m6!1e1!3m4!1sndCK9RajrKcBlS23REjO6A!2e0!7i13312!8i6656!6m1!1e1',
+			'https://www.google.ca/maps/place/Tehran,+Iran/@35.6762219,51.4221819,3a,37.5y,203.74h,96.35t/data=!3m8!1e1!3m6!1s-mNZa1NWN1OU%2FVrkQygtHAhI%2FAAAAAAAAD4g%2Fhzg6SLHKoM0!2e4!3e11!6s%2F%2Flh4.googleusercontent.com%2F-mNZa1NWN1OU%2FVrkQygtHAhI%2FAAAAAAAAD4g%2Fhzg6SLHKoM0%2Fw203-h101-n-k-no%2F!7i7168!8i3584!4m2!3m1!1s0x3f8e00491ff3dcd9:0xf0b3697c567024bc!6m1!1e1',
+			'https://www.google.ca/maps/@33.3123817,44.45092,3a,90y,206.19h,64.6t/data=!3m7!1e1!3m5!1s-9sZBSvtVpQw%2FUvj9T0-IrII%2FAAAAAAAAAAA%2FuJRLDvw05DM!2e4!3e11!7i4000!8i2000!6m1!1e1',
+			'https://www.google.ca/maps/@53.3455096,-6.2437213,3a,90y,280.1h,73.05t/data=!3m6!1e1!3m4!1sArxteFIwGoeKh51bYgQChg!2e0!7i13312!8i6656!6m1!1e1',
+			'https://www.google.ca/maps/@19.4313199,-99.1362153,3a,75y,189.34h,78.15t/data=!3m6!1e1!3m4!1sw24Nz0L7VIndhuWkvhYzpA!2e0!7i13312!8i6656!6m1!1e1',
+			'https://www.google.ca/maps/@41.883048,12.5890732,3a,90y,284.42h,71.98t/data=!3m6!1e1!3m4!1sBhles1abvVdnXPtl9BpyZQ!2e0!7i13312!8i6656',
 			'https://www.google.ca/maps/place/Juba,+South+Sudan/@4.860203,31.6048497,3a,75y,181h,90t/data=!3m8!1e2!3m6!1s-j0CsuMirsrg%2FVQslPcjKtEI%2FAAAAAAAAHdI%2FhzlPgEmE6-Y!2e4!3e12!6s%2F%2Flh5.googleusercontent.com%2F-j0CsuMirsrg%2FVQslPcjKtEI%2FAAAAAAAAHdI%2FhzlPgEmE6-Y%2Fs203-k-no%2F!7i4608!8i3456!4m2!3m1!1s0x1712804abcf3b5f9:0xd89839286346c433!6m1!1e1',
-			'https://www.google.ca/maps/place/Paramaribo,+Suriname/@5.850711,-55.120489,3a,75y,266h,90t/data=!3m8!1e2!3m6!1s57757741!2e1!3e10!6s%2F%2Flh3.googleusercontent.com%2Fproxy%2FgrdFGG7-6ZAKfqacCh4XD_b18Wt7Cx4xpoo5rMJ_es0PjQLtlcjYyX5aQ4DLf9aB5K0GOTtKQhAWuGsY2CBB2FSDrKFGYg%3Dw203-h152!7i1280!8i961!4m2!3m1!1s0x8d083520e203f743:0x97cf2079da3f4063!6m1!1e1',
-			'https://www.google.ca/maps/place/Chisinau,+Moldova/@47.0021769,28.8482689,3a,90y,256.85h,75.47t/data=!3m8!1e1!3m6!1s-OQmrQn7x_E8%2FVbNUxJ-170I%2FAAAAAAABGJc%2FJstx8KqxMXs!2e4!3e11!6s%2F%2Flh5.googleusercontent.com%2F-OQmrQn7x_E8%2FVbNUxJ-170I%2FAAAAAAABGJc%2FJstx8KqxMXs%2Fw203-h101-n-k-no%2F!7i5656!8i2828!4m2!3m1!1s0x40c97c3628b769a1:0x37d1d6305749dd3c!6m1!1e1',
-			'https://www.google.ca/maps/@55.7663773,37.6268296,3a,75y,4.94h,70.07t/data=!3m6!1e1!3m4!1s71BTcJWcclVMh2RSC5nLyA!2e0!7i13312!8i6656',
-			'https://www.google.ca/maps/@18.0029104,-76.7896973,3a,54.7y,328.16h,81.61t/data=!3m7!1e1!3m5!1s-fhO10mNzExA%2FVSbKsu_zVhI%2FAAAAAAAAAAA%2Fo53gqV2i6yI!2e4!3e11!7i2508!8i1254',
-			'https://www.google.ca/maps/place/Bandar+Seri+Begawan,+Brunei/@4.894355,114.9414989,3a,45y,346.13h,98.52t/data=!3m8!1e1!3m6!1s-pM8YgdlGQMI%2FVU8JSHFQfrI%2FAAAAAAAACTY%2FDwM7EbCv_WQ!2e4!3e11!6s%2F%2Flh4.googleusercontent.com%2F-pM8YgdlGQMI%2FVU8JSHFQfrI%2FAAAAAAAACTY%2FDwM7EbCv_WQ%2Fw203-h101-n-k-no%2F!7i4000!8i2000!4m2!3m1!1s0x3222f5b0540b731f:0xeeacf8c47cbae84b!6m1!1e1',
-			'https://www.google.ca/maps/@45.7697334,16.071068,3a,60y,217.15h,73.71t/data=!3m7!1e1!3m5!1sNHyCfPFXSlj_qBB2E8wUSQ!2e0!6s%2F%2Fgeo2.ggpht.com%2Fcbk%3Fpanoid%3DNHyCfPFXSlj_qBB2E8wUSQ%26output%3Dthumbnail%26cb_client%3Dmaps_sv.tactile.gps%26thumb%3D2%26w%3D203%26h%3D100%26yaw%3D289.44641%26pitch%3D0!7i13312!8i6656!6m1!1e1',
-			'https://www.google.ca/maps/@46.0919245,14.5126915,3a,45y,118.63h,64.45t/data=!3m6!1e1!3m4!1sWYdQk8liZdkOrDyeUHYVXQ!2e0!7i13312!8i6656',
-			'https://www.google.ca/maps/@-15.8727806,-47.9234965,3a,75y,47.01h,44.26t/data=!3m6!1e1!3m4!1sIFPFjVya6jzxruVdcxf49Q!2e0!7i13312!8i6656'
+			'https://www.google.ca/maps/@47.9116453,106.8366065,3a,37.5y,349.36h,76.1t/data=!3m6!1e1!3m4!1sgxqt3yhRsG2QoLpz6i1ddA!2e0!7i13312!8i6656!6m1!1e1',
+			'https://www.google.ca/maps/@-0.113544,-78.4654784,3a,90y,315.56h,68.49t/data=!3m6!1e1!3m4!1s2KAXujuu6xx5qLz4aURu0A!2e0!7i13312!8i6656!6m1!1e1',
+			'https://www.google.ca/maps/@37.4998815,127.1517875,3a,30y,27.25h,85t/data=!3m6!1e1!3m4!1s3bYZA_VP-5fQz3GGd44ZcA!2e0!7i13312!8i6656!6m1!1e1'
 		]
 	};
 

3bb5b8550927f032bd64cd6c0125201cfd2269d3
Vera Zabeida <vera.zabeida@gmail.com>
2016-04-13 22:43:17 -0400

    changed styles

--- staticAssets/scripts/bundle.js
+++ staticAssets/scripts/bundle.js
@@ -1,6 +1,35 @@
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.controller = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-(function (){
+var data = {
+		countries: [
+			['indonesia', 'jakarta'],
+			['iran','tehran'],
+			['iraq','baghdad'],
+			['ireland','dublin'],
+			['mexico', 'mexico city'],
+			['italy', 'rome'],
+			['south_sudan', 'juba'],
+			['mongolia', 'ulaanbaatar'],
+			['ecuador', 'quito'],
+			['south_korea', 'seoul']
+		],
+		sources: [
+			'https://www.google.ca/maps/@-6.1722729,106.8130564,3a,90y,165.99h,60.31t/data=!3m6!1e1!3m4!1sndCK9RajrKcBlS23REjO6A!2e0!7i13312!8i6656!6m1!1e1',
+			'https://www.google.ca/maps/place/Tehran,+Iran/@35.6762219,51.4221819,3a,37.5y,203.74h,96.35t/data=!3m8!1e1!3m6!1s-mNZa1NWN1OU%2FVrkQygtHAhI%2FAAAAAAAAD4g%2Fhzg6SLHKoM0!2e4!3e11!6s%2F%2Flh4.googleusercontent.com%2F-mNZa1NWN1OU%2FVrkQygtHAhI%2FAAAAAAAAD4g%2Fhzg6SLHKoM0%2Fw203-h101-n-k-no%2F!7i7168!8i3584!4m2!3m1!1s0x3f8e00491ff3dcd9:0xf0b3697c567024bc!6m1!1e1',
+			'https://www.google.ca/maps/@33.3123817,44.45092,3a,90y,206.19h,64.6t/data=!3m7!1e1!3m5!1s-9sZBSvtVpQw%2FUvj9T0-IrII%2FAAAAAAAAAAA%2FuJRLDvw05DM!2e4!3e11!7i4000!8i2000!6m1!1e1',
+			'https://www.google.ca/maps/@53.3455096,-6.2437213,3a,90y,280.1h,73.05t/data=!3m6!1e1!3m4!1sArxteFIwGoeKh51bYgQChg!2e0!7i13312!8i6656!6m1!1e1',
+			'https://www.google.ca/maps/@19.4313199,-99.1362153,3a,75y,189.34h,78.15t/data=!3m6!1e1!3m4!1sw24Nz0L7VIndhuWkvhYzpA!2e0!7i13312!8i6656!6m1!1e1',
+			'https://www.google.ca/maps/@41.883048,12.5890732,3a,90y,284.42h,71.98t/data=!3m6!1e1!3m4!1sBhles1abvVdnXPtl9BpyZQ!2e0!7i13312!8i6656',
+			'https://www.google.ca/maps/place/Juba,+South+Sudan/@4.860203,31.6048497,3a,75y,181h,90t/data=!3m8!1e2!3m6!1s-j0CsuMirsrg%2FVQslPcjKtEI%2FAAAAAAAAHdI%2FhzlPgEmE6-Y!2e4!3e12!6s%2F%2Flh5.googleusercontent.com%2F-j0CsuMirsrg%2FVQslPcjKtEI%2FAAAAAAAAHdI%2FhzlPgEmE6-Y%2Fs203-k-no%2F!7i4608!8i3456!4m2!3m1!1s0x1712804abcf3b5f9:0xd89839286346c433!6m1!1e1',
+			'https://www.google.ca/maps/@47.9116453,106.8366065,3a,37.5y,349.36h,76.1t/data=!3m6!1e1!3m4!1sgxqt3yhRsG2QoLpz6i1ddA!2e0!7i13312!8i6656!6m1!1e1',
+			'https://www.google.ca/maps/@-0.113544,-78.4654784,3a,90y,315.56h,68.49t/data=!3m6!1e1!3m4!1s2KAXujuu6xx5qLz4aURu0A!2e0!7i13312!8i6656!6m1!1e1',
+			'https://www.google.ca/maps/@37.4998815,127.1517875,3a,30y,27.25h,85t/data=!3m6!1e1!3m4!1s3bYZA_VP-5fQz3GGd44ZcA!2e0!7i13312!8i6656!6m1!1e1'
+		]
+	};
 
+module.exports = data;
+},{}],2:[function(require,module,exports){
+(function (){
+	var data = require('./data');
 	var slider = document.getElementById('fences');
 	var windowMiddle = (slider.offsetWidth)/2;
 	var end, originals, imageSize, numImagesPerPage, clones, images, selected,
@@ -17,7 +46,7 @@
 		function preloadImage(x){
 			var picture = loadImage(x);
 
-			if(x===10){
+			if(x===195){
 				picture.onload = function(){
 					fences.removeChild(loader);
 					var end = new Date().getTime();
@@ -26,7 +55,7 @@
 			}
 		};
 
-		for(var x = 1; x<=10; x++){
+		for(var x = 1; x<=195; x++){
 			preloadImage(x);
 		};
 	};
@@ -96,7 +125,7 @@
 			slider.appendChild(a);
 		};
 
-		for(var x = 1; x<=10; x++){
+		for(var x = 1; x<=195; x++){
 			createImage(x);
 		};
 
@@ -300,32 +329,7 @@
 		appendClones();
 	};
 
-	var data = {
-		countries: [
-			['indonesia', 'jakarta'],
-			['iran','tehran'],
-			['iraq','baghdad'],
-			['ireland','dublin'],
-			['mexico', 'mexico city'],
-			['italy', 'rome'],
-			['south_sudan', 'juba'],
-			['mongolia', 'ulaanbaatar'],
-			['ecuador', 'quito'],
-			['south_korea', 'seoul']
-		],
-		sources: [
-			'https://www.google.ca/maps/@-6.1722729,106.8130564,3a,90y,165.99h,60.31t/data=!3m6!1e1!3m4!1sndCK9RajrKcBlS23REjO6A!2e0!7i13312!8i6656!6m1!1e1',
-			'https://www.google.ca/maps/place/Tehran,+Iran/@35.6762219,51.4221819,3a,37.5y,203.74h,96.35t/data=!3m8!1e1!3m6!1s-mNZa1NWN1OU%2FVrkQygtHAhI%2FAAAAAAAAD4g%2Fhzg6SLHKoM0!2e4!3e11!6s%2F%2Flh4.googleusercontent.com%2F-mNZa1NWN1OU%2FVrkQygtHAhI%2FAAAAAAAAD4g%2Fhzg6SLHKoM0%2Fw203-h101-n-k-no%2F!7i7168!8i3584!4m2!3m1!1s0x3f8e00491ff3dcd9:0xf0b3697c567024bc!6m1!1e1',
-			'https://www.google.ca/maps/@33.3123817,44.45092,3a,90y,206.19h,64.6t/data=!3m7!1e1!3m5!1s-9sZBSvtVpQw%2FUvj9T0-IrII%2FAAAAAAAAAAA%2FuJRLDvw05DM!2e4!3e11!7i4000!8i2000!6m1!1e1',
-			'https://www.google.ca/maps/@53.3455096,-6.2437213,3a,90y,280.1h,73.05t/data=!3m6!1e1!3m4!1sArxteFIwGoeKh51bYgQChg!2e0!7i13312!8i6656!6m1!1e1',
-			'https://www.google.ca/maps/@19.4313199,-99.1362153,3a,75y,189.34h,78.15t/data=!3m6!1e1!3m4!1sw24Nz0L7VIndhuWkvhYzpA!2e0!7i13312!8i6656!6m1!1e1',
-			'https://www.google.ca/maps/@41.883048,12.5890732,3a,90y,284.42h,71.98t/data=!3m6!1e1!3m4!1sBhles1abvVdnXPtl9BpyZQ!2e0!7i13312!8i6656',
-			'https://www.google.ca/maps/place/Juba,+South+Sudan/@4.860203,31.6048497,3a,75y,181h,90t/data=!3m8!1e2!3m6!1s-j0CsuMirsrg%2FVQslPcjKtEI%2FAAAAAAAAHdI%2FhzlPgEmE6-Y!2e4!3e12!6s%2F%2Flh5.googleusercontent.com%2F-j0CsuMirsrg%2FVQslPcjKtEI%2FAAAAAAAAHdI%2FhzlPgEmE6-Y%2Fs203-k-no%2F!7i4608!8i3456!4m2!3m1!1s0x1712804abcf3b5f9:0xd89839286346c433!6m1!1e1',
-			'https://www.google.ca/maps/@47.9116453,106.8366065,3a,37.5y,349.36h,76.1t/data=!3m6!1e1!3m4!1sgxqt3yhRsG2QoLpz6i1ddA!2e0!7i13312!8i6656!6m1!1e1',
-			'https://www.google.ca/maps/@-0.113544,-78.4654784,3a,90y,315.56h,68.49t/data=!3m6!1e1!3m4!1s2KAXujuu6xx5qLz4aURu0A!2e0!7i13312!8i6656!6m1!1e1',
-			'https://www.google.ca/maps/@37.4998815,127.1517875,3a,30y,27.25h,85t/data=!3m6!1e1!3m4!1s3bYZA_VP-5fQz3GGd44ZcA!2e0!7i13312!8i6656!6m1!1e1'
-		]
-	};
+
 
 	aboutClick();
 	preload();
@@ -333,5 +337,5 @@
 })();
 
 
-},{}]},{},[1])(1)
+},{"./data":1}]},{},[2])(2)
 });
---
+++ staticAssets/scripts/data.js
@@ -0,0 +1,28 @@
+var data = {
+		countries: [
+			['indonesia', 'jakarta'],
+			['iran','tehran'],
+			['iraq','baghdad'],
+			['ireland','dublin'],
+			['mexico', 'mexico city'],
+			['italy', 'rome'],
+			['south_sudan', 'juba'],
+			['mongolia', 'ulaanbaatar'],
+			['ecuador', 'quito'],
+			['south_korea', 'seoul']
+		],
+		sources: [
+			'https://www.google.ca/maps/@-6.1722729,106.8130564,3a,90y,165.99h,60.31t/data=!3m6!1e1!3m4!1sndCK9RajrKcBlS23REjO6A!2e0!7i13312!8i6656!6m1!1e1',
+			'https://www.google.ca/maps/place/Tehran,+Iran/@35.6762219,51.4221819,3a,37.5y,203.74h,96.35t/data=!3m8!1e1!3m6!1s-mNZa1NWN1OU%2FVrkQygtHAhI%2FAAAAAAAAD4g%2Fhzg6SLHKoM0!2e4!3e11!6s%2F%2Flh4.googleusercontent.com%2F-mNZa1NWN1OU%2FVrkQygtHAhI%2FAAAAAAAAD4g%2Fhzg6SLHKoM0%2Fw203-h101-n-k-no%2F!7i7168!8i3584!4m2!3m1!1s0x3f8e00491ff3dcd9:0xf0b3697c567024bc!6m1!1e1',
+			'https://www.google.ca/maps/@33.3123817,44.45092,3a,90y,206.19h,64.6t/data=!3m7!1e1!3m5!1s-9sZBSvtVpQw%2FUvj9T0-IrII%2FAAAAAAAAAAA%2FuJRLDvw05DM!2e4!3e11!7i4000!8i2000!6m1!1e1',
+			'https://www.google.ca/maps/@53.3455096,-6.2437213,3a,90y,280.1h,73.05t/data=!3m6!1e1!3m4!1sArxteFIwGoeKh51bYgQChg!2e0!7i13312!8i6656!6m1!1e1',
+			'https://www.google.ca/maps/@19.4313199,-99.1362153,3a,75y,189.34h,78.15t/data=!3m6!1e1!3m4!1sw24Nz0L7VIndhuWkvhYzpA!2e0!7i13312!8i6656!6m1!1e1',
+			'https://www.google.ca/maps/@41.883048,12.5890732,3a,90y,284.42h,71.98t/data=!3m6!1e1!3m4!1sBhles1abvVdnXPtl9BpyZQ!2e0!7i13312!8i6656',
+			'https://www.google.ca/maps/place/Juba,+South+Sudan/@4.860203,31.6048497,3a,75y,181h,90t/data=!3m8!1e2!3m6!1s-j0CsuMirsrg%2FVQslPcjKtEI%2FAAAAAAAAHdI%2FhzlPgEmE6-Y!2e4!3e12!6s%2F%2Flh5.googleusercontent.com%2F-j0CsuMirsrg%2FVQslPcjKtEI%2FAAAAAAAAHdI%2FhzlPgEmE6-Y%2Fs203-k-no%2F!7i4608!8i3456!4m2!3m1!1s0x1712804abcf3b5f9:0xd89839286346c433!6m1!1e1',
+			'https://www.google.ca/maps/@47.9116453,106.8366065,3a,37.5y,349.36h,76.1t/data=!3m6!1e1!3m4!1sgxqt3yhRsG2QoLpz6i1ddA!2e0!7i13312!8i6656!6m1!1e1',
+			'https://www.google.ca/maps/@-0.113544,-78.4654784,3a,90y,315.56h,68.49t/data=!3m6!1e1!3m4!1s2KAXujuu6xx5qLz4aURu0A!2e0!7i13312!8i6656!6m1!1e1',
+			'https://www.google.ca/maps/@37.4998815,127.1517875,3a,30y,27.25h,85t/data=!3m6!1e1!3m4!1s3bYZA_VP-5fQz3GGd44ZcA!2e0!7i13312!8i6656!6m1!1e1'
+		]
+	};
+
+module.exports = data;
